a6a98fc01125dad819153d455ae5dc912044ca50
Kamilo Chinome <kamiloandresch@gmail.com>
2025-05-19 22:44:29 -0500

    sign up form impl

--- lib/auth/presentation/screens/sign_up_screen.dart
+++ lib/auth/presentation/screens/sign_up_screen.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:petto/app/router/app_router.dart';
